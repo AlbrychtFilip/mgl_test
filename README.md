7edabb0061e6f91324177c22b4b1672da83333b9
AlbrychtFilip <filip.albrycht@gmail.com>
2024-04-29 18:43:06 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 
+
+
+## Installation
+Run `npm install` command in root folder and backend folder
+```
+npm install 
+```
+## How to Run
+
+```
+npm run dev 
+```
+
+
+fixes:
+1. migration missing , in backend/src/db/new_mgldefi.sql line 213
+2. sending emails doesnt work because of wrong credentials - new values required
+3. confirm_password - no need to send that
+4. added check on verify email to not allow to verify email if already verified
+
+
+Also I added some comments in code, check falbrycht phase
---
+++ src/components/providers/PresaleProvider.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect,useCallback } from 'react';
+import axios from 'axios';
+import { useTranslation } from 'react-i18next'
+
+const PresaleContextTemplate = {
+    presaleData: String,
+    setPresaleData: (requestData) => {},
+    
+}
+const PresaleContext = React.createContext(PresaleContextTemplate);
+
+
+function PresaleProvider(props) {
+  const [presaleData, setPresaleData] = useState("");
+  // falbrycht: no effect
+  // useEffect(()=>{
+  //   console.log("presaleData",presaleData)
+  // },[presaleData])
+  return(
+          <PresaleContext.Provider value={{
+            presaleData,
+            setPresaleData
+          }}>
+            {props.children}
+          </PresaleContext.Provider>
+
+    )
+}
+
+export {PresaleContext};
+export default PresaleProvider;
